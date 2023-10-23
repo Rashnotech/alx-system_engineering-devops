f5de93f4c33904b588d23a09b49947e697950044
Rashnotech <rashnotech@gmail.com>
2023-10-23 23:44:40 +0000

    Fixed: username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -16,13 +16,13 @@ def progress(payload, params):
 
 if __name__ == '__main__':
     """ code executation """
-    id = int(argv[1])
-    emp = progress({'id': id}, 'users')
-    todos = progress({'userId': id}, 'todos')
-    filename = f'{id}.csv'
-    username = emp[0].get('name')
+    user_id = argv[1]
+    emp = progress({'id': user_id}, 'users')
+    todos = progress({'userId': user_id}, 'todos')
+    filename = f'{user_id}.csv'
+    username = emp[0].get('username')
     with open(filename, 'w', newline="") as file:
         csv_writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for todo in todos:
-            csv_writer.writerow([id, username, todo.get('completed'),
+            csv_writer.writerow([user_id, username, todo.get('completed'),
                                 todo.get('title')])
