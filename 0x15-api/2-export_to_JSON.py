d6a6dcd52f5a1a68f3716ea63e7706d7eb7add4d
Rashnotech <rashnotech@gmail.com>
2023-10-24 00:32:29 +0000

    Json version

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ a module that returns information about an employee """
-import csv
+import json
 import requests
 from sys import argv
 
@@ -17,12 +17,13 @@ def progress(payload, params):
 if __name__ == '__main__':
     """ code executation """
     user_id = argv[1]
-    emp = progress({'id': user_id}, 'users')
+    user = progress({}, f'users/{user_id}')
     todos = progress({'userId': user_id}, 'todos')
-    filename = f'{user_id}.csv'
-    username = emp[0].get('username')
-    with open(filename, 'w', newline="") as file:
-        csv_writer = csv.writer(file, quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            csv_writer.writerow([user_id, username, todo.get('completed'),
-                                todo.get('title')])
+    filename = f'user_id.json'
+    username = user['username']
+    result = {user_id: [{
+                        'task': todo.get('title'),
+                        'completed': todo.get('completed'),
+                        'username': username} for todo in todos]}
+    with open(filename, 'w') as file:
+        json.dump(result, file)
