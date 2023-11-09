cda59dd594fda57bb5f24e1bb15b8091a16d154c
Rashnotech <rashnotech@gmail.com>
2023-11-09 10:37:35 +0000

    recursive function

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,16 +3,25 @@
 from requests import get
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """
         A function that queries an API
         Args:
             subreddit (str): subreddit articles title
-            host_list (list): 
+            host_list (list): A list to store the titles of hot articles
         Returns:
             list: a list containing the titles of all hot articles
     """
+    headers = {'User-Agent': 'MyRedditScraper/1.0'}
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    reponse = get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    param = {'limit': 100, 'after': after}
+    response = get(url, headers=headers, params=param, allow_redirects=False)
+    try:
         data = response.json().get('data')
+        if data['after']:
+            return recurse(subreddit, hot_list, data['after'])
+        for val in data['children']:
+            hot_list.append(val['data']['title'])
+        return hot_list
+    except Exception:
+        return None
